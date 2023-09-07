fa81a15da9ef810c670b30ecd6ef25516e48f663
Emmanuel odii <emmanuelodii80@gmail.com>
2023-09-07 14:42:33 +0100

    chore: add singleton prisma and more data to seed db

--- packages/app/prisma/cars.ts
+++ packages/app/prisma/cars.ts
@@ -1,7 +1,7 @@
 export const Cars = [
   {
-    model: "McLaren",
-    make: "speedtail",
+    make: "McLaren",
+    model: "speedtail",
     year: 2020,
     weekend: {
       create: [
@@ -40,15 +40,95 @@ export const Cars = [
     dougscore: 74,
   },
   {
-    model: "McLaren",
-    make: "speedtail",
-    year: 2020,
+    make: "McLaren",
+    model: "F1",
+    year: 1994,
+    weekend: {
+      create: [
+        {
+          styling: 8,
+          acceleration: 9,
+          handling: 10,
+          funFact: 10,
+          coolFact: 10,
+          total: 47,
+        },
+      ],
+    },
+    daily: {
+      create: [
+        {
+          features: 2,
+          comfort: 3,
+          quality: 9,
+          practical: 3,
+          value: 10,
+          total: 27,
+        },
+      ],
+    },
+    other: {
+      create: [
+        {
+          country: "Carlifonia",
+          town: "Burbank",
+          vehicleCountry: "UK",
+          videoLink: "https://youtube.com/1213",
+        },
+      ],
+    },
+    dougscore: 74,
+  },
+  {
+    make: "Bugatti",
+    model: "Divo",
+    year: 2019,
     weekend: {
       create: [
         {
           styling: 8,
           acceleration: 10,
-          handling: 10,
+          handling: 9,
+          funFact: 10,
+          coolFact: 10,
+          total: 47,
+        },
+      ],
+    },
+    daily: {
+      create: [
+        {
+          features: 6,
+          comfort: 5,
+          quality: 7,
+          practical: 1,
+          value: 7,
+          total: 26,
+        },
+      ],
+    },
+    other: {
+      create: [
+        {
+          country: "FLorida",
+          town: "Palm beach",
+          vehicleCountry: "France",
+          videoLink: "https://youtube.com/1214",
+        },
+      ],
+    },
+    dougscore: 73,
+  },
+  {
+    make: "Bugatti",
+    model: "Chiron Pur Sport",
+    year: 2021,
+    weekend: {
+      create: [
+        {
+          styling: 9,
+          acceleration: 10,
+          handling: 9,
           funFact: 10,
           coolFact: 10,
           total: 48,
@@ -58,26 +138,65 @@ export const Cars = [
     daily: {
       create: [
         {
-          features: 7,
-          comfort: 4,
+          features: 6,
+          comfort: 5,
           quality: 7,
-          practical: 2,
+          practical: 1,
           value: 6,
-          total: 26,
+          total: 25,
         },
       ],
     },
     other: {
       create: [
         {
-          country: "Columbus",
-          town: "Ohio",
-          vehicleCountry: "UK",
-          videoLink: "https://youtube.com/1212",
+          country: "Carlifonia",
+          town: "Newport beach",
+          vehicleCountry: "France",
+          videoLink: "https://youtube.com/1215",
         },
       ],
     },
-    dougscore: 74,
+    dougscore: 73,
   },
-  // ...
+  {
+    make: "Bugatti",
+    model: "Chiron",
+    year: 2018,
+    weekend: {
+      create: [
+        {
+          styling: 9,
+          acceleration: 10,
+          handling: 9,
+          funFact: 10,
+          coolFact: 10,
+          total: 48,
+        },
+      ],
+    },
+    daily: {
+      create: [
+        {
+          features: 6,
+          comfort: 5,
+          quality: 7,
+          practical: 1,
+          value: 6,
+          total: 25,
+        },
+      ],
+    },
+    other: {
+      create: [
+        {
+          country: "Toronto",
+          town: "Ontario",
+          vehicleCountry: "France",
+          videoLink: "https://youtube.com/1216",
+        },
+      ],
+    },
+    dougscore: 73,
+  },
 ];
