ab9239d9a233844eb806ab567303a76648be992f
Emmanuel odii <emmanuelodii80@gmail.com>
2023-09-12 17:15:38 +0100

    fix: add imports from index in dev branch

--- packages/app/src/app/car/page.tsx
+++ packages/app/src/app/car/page.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Page = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  }, [router]);
   return (
     <div className="min-h-screen container mx-auto border py-12 max-w-xl">
       Page
