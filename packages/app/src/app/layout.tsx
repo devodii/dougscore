ef15002e8476c5e7df67423ed69a4b2b2677beda
Emmanuel odii <emmanuelodii80@gmail.com>
2023-09-13 16:11:49 +0100

    feat: add basic metadata

--- packages/app/src/app/layout.tsx
+++ packages/app/src/app/layout.tsx
@@ -4,8 +4,23 @@ import type { Metadata } from "next";
 import { ModalProvider } from "@/components/context/modal.context";
 
 export const metadata: Metadata = {
-  title: "Dougscore",
-  description: "Get dougreview on car searches",
+  title: "Dougscore finder",
+  description:
+    "Explore expert car reviews and ratings. Discover your dream car with Dougscore ratings and compare vehicles effortlessly. Your trusted source for car research.",
+  keywords: [
+    "cars",
+    "car reviews",
+    "car sale",
+    "cars",
+    "new cars",
+    "cars in the world",
+    "car score",
+    "used cars",
+  ],
+  robots: "index, follow",
+  referrer: "origin",
+  creator: "Emmanuel Odii",
+  viewport: { width: "device-width", initialScale: 1 },
 };
 
 export default function RootLayout({
