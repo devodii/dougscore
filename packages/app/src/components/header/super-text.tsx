9bf4f77858c90fd370b5bf8940698d5bafe62024
Emmanuel odii <emmanuelodii80@gmail.com>
2023-09-28 12:10:34 +0100

    chore: change ownership to odii

--- packages/app/src/components/header/super-text.tsx
+++ packages/app/src/components/header/super-text.tsx
@@ -6,12 +6,12 @@ const SuperText = () => {
     <div className="text-[#28633a] text-bases font-semibold text-center py-1 font-sans">
       Influence free. Powered by{" "}
       <Link
-        href="http://www.dougdemuro.com/"
+        href="http://www.twitter.com/devodii_"
         target="_blank"
         className="underline underline-offset-2"
       >
         {" "}
-        Doug DeMuro
+        Emmanuel Odii
       </Link>
     </div>
   );
