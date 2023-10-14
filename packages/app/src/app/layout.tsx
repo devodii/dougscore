1565581e9da5d08a969eefb3c663f5d56ae30b4a
Emmanuel odii <emmanuelodii80@gmail.com>
2023-10-14 19:38:18 +0100

    chore: fix google analytics

--- packages/app/src/app/layout.tsx
+++ packages/app/src/app/layout.tsx
@@ -47,6 +47,12 @@ export default function RootLayout({
           });
        `}
       </Script>
+
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2642908073199820"
+        crossOrigin="anonymous"
+      />
       <meta name="google-adsense-account" content="ca-pub-2642908073199820" />
       <body>
         <ModalProvider>
